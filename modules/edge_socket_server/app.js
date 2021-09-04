d9495c0603a8e7ef3bfa69e04238519e9208870d
Sineth Fernando <SinethF@jkintranet.com>
2021-09-04 08:46:52 +0530

    socket io exposed port correction

--- modules/edge_socket_server/app.js
+++ modules/edge_socket_server/app.js
@@ -4,47 +4,53 @@ var Transport = require('azure-iot-device-mqtt').Mqtt;
 var Client = require('azure-iot-device').ModuleClient;
 var Message = require('azure-iot-device').Message;
 let app = require('express')();
-const http = require('http');
-const socket = require('socket.io');
-const server = http.createServer();
-const port = 11002;
-const prefix = "V2: ";
-const nspPrefix = "/nsp," + prefix;
-
-let counter = 0;
+let http = require('http').createServer(app);
+let io = require('socket.io')(http);
 
 io.on('connection', (socket) => {
   console.log('USER CONNECTED');
 
-  socket.on('disconnect', function(){
+  socket.on('disconnect', function () {
     console.log('USER DISCONNECTED');
   });
- 
+
 });
 
-//Iot device mqtt connection  
-Client.fromEnvironment(Transport, function (err, client) {
-  if (err) {
-    throw err;
-  } else {
-    client.on('error', function (err) {
+app.get('/',(req,res)=>{
+  console.info(req);
+  console.info(res);
+  res.send('Hello World- From Edge Node js server')
+});
+
+http.listen(8092, () => {
+
+  console.info('started server on port - 8092');
+
+  //Iot device mqtt connection  
+  Client.fromEnvironment(Transport, function (err, client) {
+    if (err) {
       throw err;
-    });
-
-    // connect to the Edge instance
-    client.open(function (err) {
-      if (err) {
+    } else {
+      client.on('error', function (err) {
         throw err;
-      } else {
-        console.log('IoT Hub module client initialized');
+      });
 
-        // Act on input messages to the module.
-        client.on('inputMessage', function (inputName, msg) {
-          pipeMessage(client, inputName, msg);
-        });
-      }
-    });
-  }
+      // connect to the Edge instance
+      client.open(function (err) {
+        if (err) {
+          throw err;
+        } else {
+          console.log('IoT Hub module client initialized');
+
+          // Act on input messages to the module.
+          client.on('inputMessage', function (inputName, msg) {
+            pipeMessage(client, inputName, msg);
+          });
+        }
+      });
+    }
+  });
+
 });
 
 // This function just pipes the messages without any change.
@@ -57,7 +63,7 @@ function pipeMessage(client, inputName, msg) {
       //var outputMsg = new Message(message);
       // client.sendOutputEvent('output1', outputMsg, printResultFor('Sending received message'));
       //Sending the data to client web app (emit data)
-      io.emit('bmsWebApp', {x:(new Date()).getTime(), y: message});
+      io.emit('bmsWebApp', { x: (new Date()).getTime(), y: message });
       //console.log(message)
       console.log('new value emmited ;-)')
     }
