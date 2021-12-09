70baf0fed8b30282f73999022afc106c404fd35d
Sineth Fernando <SinethF@jkintranet.com>
2021-12-09 14:49:27 +0530

    changes from kapacitor tensorflowk keras udf

--- modules/edge_influx_kapacitor/udfs/udf_fdd.py
+++ modules/edge_influx_kapacitor/udfs/udf_fdd.py
@@ -1,7 +1,8 @@
 from kapacitor.udf.agent import Agent, Handler
 from kapacitor.udf import udf_pb2
-import logging
+from tensorflow.keras.layers import LSTM, Dense
 import json
+import logging
 
 logging.basicConfig(level=logging.DEBUG,format='%(asctime)s %(levelname)s:%(name)s: %(message)s')
 logger = logging.getLogger()
@@ -10,7 +11,7 @@ class FddPredictorHandler(Handler):
     def __init__(self,agent):
         # Define what your UDF wants and what will it provides and options
         logger.info('__init__trigger')
-        self.__agent = agent
+        self._agent = agent
         self._field = ''
         self._size = 10
         self._points = []
@@ -22,6 +23,7 @@ class FddPredictorHandler(Handler):
         response.info.wants = udf_pb2.BATCH
         response.info.provides = udf_pb2.STREAM
         response.info.options['field'].valueTypes.append(udf_pb2.STRING)
+        response.info.options['size'].valueTypes.append(udf_pb2.INT)
         return response
 
     def init(self,init_req):
@@ -29,51 +31,80 @@ class FddPredictorHandler(Handler):
         logger.info('INIT trigger')
         for opt in init_req.options:
             if opt.name == 'field':
+                logger.info(opt.values)
                 self._field = opt.values[0].stringValue
-
+            elif opt.name == 'size':
+                logger.info(opt.values)
+                self._size = opt.values[0].intValue
+                
+        logger.info('------------------------Start - UDF - Fields--------------------------------------')
+        # logger.info(self._field,self._size)
+        logger.info('------------------------End - UDF - Fields--------------------------------------')
         success = True
         msg = ''
         if self._field == '':
             success = False
-            msg = 'A field name must be provided'
+            msg = 'must provide field name'
         response =udf_pb2.Response()
         response.init.success = success
         response.init.error = msg.encode()
+        logger.info(response)
         return response
         
 
-    def begin_batch(self):
+    def begin_batch(self, begin_req):
         # Do something at the beginning of the batch
-        logger.info('begin_batch trigger')
+        logger.info("************** Hello BEGIN BATCH********")
+        response = udf_pb2.Response()
+        response.begin.CopyFrom(begin_req)
+        self._begin_response = response
+        logger.info(begin_req)
+        logger.info("************** ByeBye BEGIN BATCH******")
 
-    # def snapshot(self):
-    #     # Take a snapshot of the current data, if the task stops for some reason
-    #     logger.info('snapshot trigger')
-    #     data = {}
-    #     for group, state in self._state.items():
-    #         data[group] = state.snapshot()
-    #     response = udf_pb2.Response()
-    #     response.snapshot.snapshot = json.dumps(data).encode()
-    #     return response
+    def snapshot(self):
+        # Take a snapshot of the current data, if the task stops for some reason
+        logger.info('snapshot trigger')
+        data = {}
+        for group, state in self._state.items():
+            data[group] = state.snapshot()
+        response = udf_pb2.Response()
+        response.snapshot.snapshot = json.dumps(data).encode()
+        #logger.info(data)
+        return response
+
+    def restore(self, restore_req):
+        logger.info('----------------------------------Restore trigger----------------------------------')
+        response = udf_pb2.Response()
+        response.restore.success = True
+        response.restore.error = ''
+        return response
+
 
     def point(self,point):
         # process each point within the batch
-        logger.info('point trigger')
+        logger.info('************** Hello Point')
         logger.info(point)
         self._points.append(point)
         if len(self._points) == self._size:
             response = udf_pb2.Response()
             response.point.name = 'point test name'
-            response.point.time = 'point test time'
+            response.point.time = point.time
             response.point.group = 'point test group'
             response.point.fieldsString['fault'] = 'test fault'
+            logger.info('**************** point response **************')
+            logger.info(response)
             self._agent.write_response(response)
+            logger.info('**************** point response after writing************')
             self._points.pop(0)
-        pass
+        logger.info("************** ByeBye Point")
+
+        
     
-    def end_batch(self):
+    def end_batch(self, end_req):
         # Do something at the end of the batch
-        logger.info('end_batch')
+        logger.info("************** Hello END BATCH")
+        logger.info(end_req)
+        logger.info("************** ByeBye END BATCH")
 
 if __name__=='__main__':
     # Agent creation
@@ -84,5 +115,6 @@ if __name__=='__main__':
     kapAgent.start()
     kapAgent.wait()
     logger.info('Agent Finished')
+
 
 
